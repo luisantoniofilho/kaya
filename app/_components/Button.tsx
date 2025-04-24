15649b987baa08c189af0bfcd18e76b5c8a3a430
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-24 13:49:40 -0300

    style: change the focus color from cyan-800 to cyan-700 on the BUtton component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -15,7 +15,7 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className={`mt-4 w-full cursor-pointer rounded-2xl px-4 py-2 text-xl text-white transition-all hover:bg-cyan-800 hover:font-bold md:text-2xl ${disable ? "bg-stone-500" : "bg-cyan-600"}`}
+      className={`mt-4 w-full cursor-pointer rounded-2xl px-4 py-2 text-xl text-white transition-all hover:bg-cyan-700 hover:font-bold md:text-2xl ${disable ? "bg-stone-500" : "bg-cyan-600"}`}
       type={type}
       disabled={disable ? disable : false}
       onClick={onClick}
