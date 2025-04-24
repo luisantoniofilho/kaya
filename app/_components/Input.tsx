28eed944c0864df15f4d9970db4383c09d57d6bd
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-24 14:00:11 -0300

    fix: include the name property on the Input component

--- app/_components/Input.tsx
+++ app/_components/Input.tsx
@@ -31,6 +31,7 @@ export default function Input({
           required
           type={type}
           placeholder={placeholder}
+          name={name}
           className={`w-full rounded-md border border-gray-300 p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none ${optionalClassName}`}
         />
       )}
