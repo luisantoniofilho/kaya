0915c55bb477752564fe92cf24c9f54d80a6d745
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-23 23:09:41 -0300

    feat: add googleusercontent to allowed domains on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
-
-export default nextConfig;
