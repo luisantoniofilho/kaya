bc5daa84c561d7fbfbb0ab9303597cebbd314468
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-24 13:25:11 -0300

    feat: show the user image on the LoginIcon component if it is logged

--- app/_components/LoginIcon.tsx
+++ app/_components/LoginIcon.tsx
@@ -1,14 +1,34 @@
 import { Dispatch, SetStateAction } from "react";
 import { HeaderLink } from "./HeaderLink";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import SpinnerMini from "./SpinnerMini";
 
 export function LoginIcon({
   onClick,
 }: {
   onClick?: Dispatch<SetStateAction<boolean>>;
 }) {
+  const { data: session, status } = useSession();
+
+  console.log(session);
   return (
-    <HeaderLink onClick={() => onClick?.(false)} href="/userLogin">
-      Login
+    <HeaderLink href="/userLogin">
+      {status === "loading" ? (
+        // Loading
+        <SpinnerMini />
+      ) : session ? (
+        <Image
+          className="rounded-full"
+          src={session.user?.image ?? "/default-avatar.png"}
+          alt="User"
+          width={25}
+          height={25}
+          onClick={() => onClick?.((prev) => !prev)}
+        />
+      ) : (
+        "Login"
+      )}
     </HeaderLink>
   );
 }
