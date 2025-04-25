eaef55843ea3e571e8f5e84903e552b0177086c2
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-24 23:21:26 -0300

    feat: create a schema for the product using zod

---
+++ app/schemas/productSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { PRODUCT_CATEGORIES } from "../constants/productCategories";
+
+export const productSchema = z.object({
+  title: z.string().min(3, "Título muito curto"),
+  description: z.string().min(3, "Descrição muito curta"),
+  category: z.enum(PRODUCT_CATEGORIES, {
+    errorMap: () => ({ message: "Selecione uma categoria válida" }),
+  }),
+  price: z.coerce
+    .number({
+      invalid_type_error: "Digite um número válido",
+    })
+    .positive("O preço não pode ser negativo"),
+});
+
+export type ProductType = z.infer<typeof productSchema>;
