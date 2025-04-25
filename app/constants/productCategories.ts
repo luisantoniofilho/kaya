a122bcfff9b324f19d4bda96a9c766fbdc7b3cc4
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-24 23:22:11 -0300

    feat: create a constant PRODUCT_CATEGORIES

---
+++ app/constants/productCategories.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES = [
+  "Maquina industrial",
+  "Eletrodoméstico",
+  "Utensílios",
+  "Material de obra",
+  "Ferramentas",
+  "Matéria prima",
+  "Mobília",
+  "Papelaria",
+  "Roupas",
+  "Equipamentos",
+  "Componentes eletrônicos",
+] as const;
