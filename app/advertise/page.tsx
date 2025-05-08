cf6ce80dac57074199863ed81d89c3c6d65e6cf2
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-08 19:54:34 -0300

    feat: check if user is authenticated on advertise page. If not, redirect to login page

--- app/advertise/page.tsx
+++ app/advertise/page.tsx
@@ -1,6 +1,12 @@
+import { redirect } from "next/navigation";
 import AdvertiseProductForm from "../_components/AdvertiseProductForm";
+import { auth } from "../_lib/auth";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  console.log(session);
+  if (!session) redirect("/login");
+
   return (
     <main className="mx-auto max-w-3xl rounded-lg bg-white p-6 shadow-md">
       <h1 className="mb-6 text-3xl font-bold text-gray-800">
