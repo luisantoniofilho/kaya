b649f279703e4dc47ce1fc69fc6b3d572d5aa360
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-09 00:43:17 -0300

    docs: add a comment on the blobActions.ts

--- app/_lib/blobActions.ts
+++ app/_lib/blobActions.ts
@@ -3,6 +3,7 @@
 import { put } from "@vercel/blob";
 
 export async function uploadImage(imageFile: File) {
+  // Upload the product images to a storage
   const blob = await put(imageFile.name, imageFile, {
     access: "public",
     addRandomSuffix: true,
