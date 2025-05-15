c97deaf4a605bb1417423d41dacef130bbb4d2b1
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-15 00:31:51 -0300

    feat: create a LinkButton reusable component

---
+++ app/_components/LinkButton.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  href: string;
+  optionalClassname?: string;
+};
+
+export default function LinkButton({
+  children,
+  href,
+  optionalClassname,
+}: ButtonProps) {
+  return (
+    <Link
+      className={`mt-4 flex w-full cursor-pointer flex-col items-center rounded-2xl bg-cyan-600 px-4 py-2 text-center text-xl text-white transition-all hover:bg-cyan-700 hover:font-bold md:text-2xl ${optionalClassname}`}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
