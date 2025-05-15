69652c10430501956f2e87517ad5a817699199c3
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-15 00:47:20 -0300

    feat: add a getProduct action to mondogbActions that get a especific product with the productId

--- app/_lib/mongodb/mongodbActions.ts
+++ app/_lib/mongodb/mongodbActions.ts
@@ -1,6 +1,7 @@
 import { ProductType } from "@/app/schemas/productSchema";
 import { UserType } from "@/app/schemas/userSchema";
 import client from "./mongodbConfig";
+import { ObjectId } from "mongodb";
 
 // Database
 const db = client.db("kaya");
@@ -10,12 +11,14 @@ const usersCollection = db.collection("users");
 const productsCollection = db.collection("products");
 
 export async function addUser(user: UserType) {
+  // Add a user to the usersCollection
   const result = await usersCollection.insertOne(user);
 
   if (!result) throw new Error("User was not created");
 }
 
 export async function getUser(userEmail: string) {
+  // Get the user with the userEmail
   const user = await usersCollection.findOne({ email: userEmail });
 
   return user;
@@ -27,7 +30,15 @@ export async function addProduct(product: ProductType) {
 }
 
 export async function getProducts() {
-  const products = productsCollection.find({}).toArray();
+  // Get all the products from productsCollection
+  const products = await productsCollection.find({}).toArray();
 
   return products;
+}
+
+export async function getProduct(productId: ObjectId) {
+  // Get a specific product with productId
+  const product = await productsCollection.findOne({ _id: productId });
+
+  return product;
 }
