f1f67ec0dc737c609f4051377668f052c564b9bc
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-15 00:38:05 -0300

    feat: create a dynamic route for the productId and show the product details on it

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,53 @@
+import getProductAction from "@/app/_lib/actions";
+import Image from "next/image";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  // Get the productId from the URL
+  const { productId } = await params;
+
+  const product = await getProductAction(productId);
+
+  if (!product) return;
+
+  return (
+    <main className="mx-auto max-w-4xl p-6">
+      {/* Product image */}
+      <div className="flex flex-col gap-8 md:flex-row">
+        <div className="flex-1">
+          <Image
+            src={product.imagePath}
+            alt={product.title}
+            height={1000}
+            width={1000}
+            className="w-full rounded-lg object-cover shadow-lg"
+          />
+        </div>
+
+        {/* Product informations */}
+        <div className="flex-1">
+          <h1 className="text-3xl font-bold text-gray-800">{product.title}</h1>
+          <h3 className="text-base font-bold text-gray-800">
+            {product.category}
+          </h3>
+          <p className="mt-4 text-2xl font-semibold text-blue-600">
+            R$ {product.price.toFixed(2)}
+          </p>
+          <p className="mt-2 text-gray-700">{product.description}</p>
+
+          <h2 className="mt-2 text-xl text-gray-700">
+            Contacte o vendedor: {product.contact}
+          </h2>
+
+          {/* Buy button */}
+          <button className="mt-6 w-full cursor-pointer rounded-lg bg-blue-600 py-3 text-white transition hover:bg-blue-700">
+            Comprar Agora
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
