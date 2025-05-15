b3f9acca33f321d8bd563f9b9fc4c776a8d007a5
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-15 01:10:48 -0300

    refactor: use getProductsAction in Top3Products component

--- app/_components/Top3Products.tsx
+++ app/_components/Top3Products.tsx
@@ -1,14 +1,21 @@
-import { getProducts } from "../_lib/mongodb/mongodbActions";
+import { getProductsAction } from "../_lib/actions";
 import Product from "./Product";
 
 export default async function Top3Products() {
-  const products = await getProducts();
+  const products = await getProductsAction();
+
+  // Check if there is an error
+  if (!Array.isArray(products)) {
+    console.error(products.error.formErrors);
+    return;
+  }
 
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       {products.slice(0, 3).map((product, index) => (
         <Product
           key={index}
+          id={product.id}
           description={product.description}
           category={product.category}
           price={product.price}
