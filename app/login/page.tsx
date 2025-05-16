9a8f3acd97111b74a9faaf5388bdd9720fd3227c
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-16 11:27:19 -0300

    fix: change the text from english to portuguese

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
       <div className="flex w-full max-w-md flex-col items-center rounded-2xl bg-white p-8 shadow-lg sm:max-w-lg md:max-w-xl md:p-12">
         {!session && (
           <div className="mb-4 w-full rounded-lg bg-blue-50 p-3 text-center text-blue-700">
-            ⚠️ Log In to access all website content!
+            ⚠️ Faça o login para acessar todo o conteúdo do site
           </div>
         )}
 
@@ -22,17 +22,17 @@ export default async function Page() {
           <>
             <h2 className="mb-4 text-center text-3xl font-bold">Welcome!</h2>
             <p className="mb-6 text-center text-base text-gray-600 md:text-lg">
-              Sign in or create an account
+              Login ou criar uma conta
             </p>
             <SigninButton />
           </>
         ) : (
           <>
             <h2 className="mb-4 text-center text-3xl font-bold">
-              You are already logged in!
+              Você ja está logado!
             </h2>
             <p className="mb-6 text-center text-base text-gray-600 md:text-lg">
-              Want to logout?
+              Sair da conta
             </p>
             <SignoutButton />
           </>
