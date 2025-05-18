e2802ff22324faacf5d1f5b48df84aceb2e2372e
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-05-17 21:22:39 -0300

    refactor: change Product component name to ProductCard

---
+++ app/_components/ProductCard.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { ProductType } from "../schemas/productSchema";
+import LinkButton from "./LinkButton";
+
+export default function ProductCard({ product }: { product: ProductType }) {
+  if (!product.imageUrl) throw new Error("Imagem sem URL");
+
+  return (
+    <div className="flex flex-col overflow-hidden rounded-2xl border border-stone-200 bg-white shadow-lg transition hover:shadow-md">
+      {/* product image */}
+      <Image
+        src={product.imageUrl}
+        alt={product.title}
+        height={500}
+        width={500}
+        className="h-48 w-full object-cover"
+      />
+
+      {/* Card content */}
+      <div className="p-4">
+        <h3 className="text-lg font-semibold text-stone-800">
+          {product.title}
+        </h3>
+        <p className="mt-1 text-stone-600">R$ {product.price}</p>
+        <p className="mt-1 text-stone-600">{product.category}</p>
+        <p className="mt-1 text-stone-600">{product.description}</p>
+
+        <LinkButton href={`/products/${product.id}`}>Ver detalhes</LinkButton>
+      </div>
+    </div>
+  );
+}
