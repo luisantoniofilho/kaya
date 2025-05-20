f67a39c062a8a8550d61a69b4ca0a49dbbf45e5e
Luis Antonio <luissantosfilho2014@gmail.com>
2025-05-20 20:26:48 -0300

    fix: update userSchema to zodv4

--- app/schemas/userSchema.ts
+++ app/schemas/userSchema.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { z } from "zod/v4";
 
 export const userSchema = z.object({
   id: z.string().length(24).optional(),
-  name: z.string().min(2, "Forneça um nome válido"),
-  email: z.string().min(1, "Forneça um email válido"),
+  name: z.string().min(2, { error: "Forneça um nome válido" }),
+  email: z.string().min(1, { error: "Forneça um email válido" }),
 });
 
 export type UserType = z.infer<typeof userSchema>;
