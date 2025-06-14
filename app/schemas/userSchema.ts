85f107eb9a747dfce28c5748e40095183a6ff18c
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-06-14 15:12:25 -0300

    feat: update the schema to neon db

--- app/schemas/userSchema.ts
+++ app/schemas/userSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod/v4";
 
 export const userSchema = z.object({
-  id: z.string().length(24).optional(),
+  id: z.number().int().optional(),
   name: z.string().min(2, { error: "Forneça um nome válido" }),
   email: z.string().min(1, { error: "Forneça um email válido" }),
 });
