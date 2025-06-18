434050973791bbb38cd91d86f8077373e23745a2
Luis Antonio <luissantosfilho2014@gmail.com>
2025-06-18 20:22:29 -0300

    refactor: add   defaultValue prop to Select component

--- app/_components/Select.tsx
+++ app/_components/Select.tsx
@@ -2,20 +2,26 @@ type SelectProps = {
   name: string;
   label: string;
   options: string[];
+  defaultValue?: string;
 };
 
 function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-export default function Select({ name, label, options }: SelectProps) {
+export default function Select({
+  name,
+  label,
+  options,
+  defaultValue,
+}: SelectProps) {
   return (
     <div>
       <label className="block font-medium text-gray-700">{label}</label>
       <select
         name={name}
         className="mt-1 w-full rounded-lg border border-gray-300 p-2 outline-none focus:ring-2 focus:ring-blue-500"
-        defaultValue=""
+        defaultValue={defaultValue ?? ""}
       >
         <option disabled value="">
           Selecione uma opção
