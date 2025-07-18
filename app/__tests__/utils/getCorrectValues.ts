8b76afb863517e437371a12c8202ef3d2509685b
Luis Antonio <luissantosfilho2014@gmail.com>
2025-07-18 18:28:05 -0300

    feat: create function getCorrectValues from form

---
+++ app/__tests__/utils/getCorrectValues.ts
@@ -0,0 +1,8 @@
+// Example with correct values to test form
+export const getCorrectValues = {
+  correctTitle: "Mesa de madeira",
+  correctDescription: "Mesa de madeira com 2 anos de uso",
+  correctCategory: "Mobília",
+  correctPrice: "500",
+  correctPhone: "11912345678",
+};
