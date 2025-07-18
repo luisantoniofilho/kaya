67605dd7413e52a1f7201f44af3c94fcefc29eab
Luis Antonio <luissantosfilho2014@gmail.com>
2025-07-18 18:29:25 -0300

    refactor: change submitButton to accept edit text

--- app/__tests__/utils/getFormElements.ts
+++ app/__tests__/utils/getFormElements.ts
@@ -2,14 +2,18 @@ import { screen } from "@testing-library/react";
 
 export const getFormElements = () => {
   return {
-    titleInput: screen.getByRole("textbox", { name: /título/i }),
+    titleInput: screen.getByRole("textbox", {
+      name: /título/i,
+    }),
     descriptionInput: screen.getByRole("textbox", {
       name: /descrição/i,
     }),
-    phoneInput: screen.getByRole("textbox", { name: /telefone/i }),
     categorySelect: screen.getByRole("combobox", { name: /categoria/i }),
     priceInput: screen.getByRole("spinbutton", { name: /preço/i }),
     imageInput: screen.getByLabelText(/imagem/i),
-    submitButton: screen.getByRole("button", { name: /anunciar/i }),
+    phoneInput: screen.getByRole("textbox", { name: /telefone/i }),
+    submitButton: screen.getByRole("button", {
+      name: /anunciar produto|confirmar alterações/i,
+    }),
   };
 };
