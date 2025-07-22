5c9ae53bbb1a6c47859dc21dda7d370fca39ac13
Luis Antonio <luissantosfilho2014@gmail.com>
2025-07-22 11:03:03 -0300

    feat: add dataTest prop handling to LinkButton for e2e testing

--- app/_components/ui/LinkButton.tsx
+++ app/_components/ui/LinkButton.tsx
@@ -5,17 +5,20 @@ type ButtonProps = {
   children: ReactNode;
   href: string;
   optionalClassname?: string;
+  dataTest?: string;
 };
 
 export default function LinkButton({
   children,
   href,
   optionalClassname,
+  dataTest,
 }: ButtonProps) {
   return (
     <Link
       className={`mt-4 flex w-full cursor-pointer flex-col items-center rounded-2xl bg-cyan-600 px-4 py-2 text-center text-xl text-white transition-all hover:bg-cyan-700 hover:font-bold md:text-2xl ${optionalClassname}`}
       href={href}
+      data-test={dataTest}
     >
       {children}
     </Link>
