2beaaf2d248c3040251af8613e1740de2e9f6e77
Luis Antonio <luissantosfilho2014@gmail.com>
2025-07-22 14:19:55 -0300

    test: create file to test view products flow

---
+++ cypress/e2e/view-product.cy.ts
@@ -0,0 +1,15 @@
+describe("test view products flow", () => {
+  it("should see products and an individual product", () => {
+    cy.visit("/products");
+    cy.wait(1000);
+    cy.get("[data-test='search-input']").type("Estante");
+
+    cy.get("[data-test='product-card-7']").should("be.visible").as("product");
+
+    cy.get("@product").should("be.visible");
+
+    cy.get("[data-test='view-details-button-7']")
+      .should("be.visible")
+      .as("viewDetailsButton");
+  });
+});
