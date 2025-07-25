be1dc85f70a015e7a9d58e9f8ae74fec5e0a881a
Luis Antonio <luissantosfilho2014@gmail.com>
2025-07-25 10:54:37 -0300

    chore: add data-test to header links

--- app/_components/ui/MobileLink.tsx
+++ app/_components/ui/MobileLink.tsx
@@ -11,7 +11,12 @@ export function MobileLink({ href, children, onClick }: MobileLinkProps) {
     "hover:bg-primary-900 rounded-full px-4 py-2 text-xl text-stone-600 transition-all duration-300 hover:bg-stone-600 hover:text-cyan-200 lg:text-lg";
 
   return (
-    <Link href={href} className={linkClass} onClick={onClick}>
+    <Link
+      href={href}
+      className={linkClass}
+      onClick={onClick}
+      data-test={`link-${href.replace("/", "")}`}
+    >
       {children}
     </Link>
   );
