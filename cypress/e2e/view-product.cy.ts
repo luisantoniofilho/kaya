075caee69ccfced81d08b633b503e21e4b1cb19c
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-07-24 21:01:10 -0300

    test: ensure products can be searched and viewed in detail

--- cypress/e2e/view-product.cy.ts
+++ cypress/e2e/view-product.cy.ts
@@ -4,6 +4,8 @@ describe("test view products flow", () => {
     cy.wait(1000);
     cy.get("[data-test='search-input']").type("Estante");
 
+    cy.get("[data-test='search-button']").click();
+
     cy.get("[data-test='product-card-7']").should("be.visible").as("product");
 
     cy.get("@product").should("be.visible");
@@ -11,5 +13,9 @@ describe("test view products flow", () => {
     cy.get("[data-test='view-details-button-7']")
       .should("be.visible")
       .as("viewDetailsButton");
+
+    cy.get("@viewDetailsButton").click();
+
+    cy.url().should("contain", "/products/7");
   });
 });
