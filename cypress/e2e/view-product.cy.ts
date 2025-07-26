3f0f21bd61520ed01ba0426e3ebd9aa60a4bb2b5
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-07-25 21:44:47 -0300

    refactor: remove cy.wait on view-products e2e test

--- cypress/e2e/view-product.cy.ts
+++ cypress/e2e/view-product.cy.ts
@@ -1,21 +1,14 @@
 describe("test view products flow", () => {
   it("should see products and an individual product", () => {
     cy.visit("/products");
-    cy.wait(1000);
     cy.get("[data-test='search-input']").type("Estante");
-
     cy.get("[data-test='search-button']").click();
-
     cy.get("[data-test='product-card-7']").as("product");
-
     cy.get("@product").should("be.visible");
-
     cy.get("[data-test='view-details-button-7']")
       .should("be.visible")
       .as("viewDetailsButton");
-
     cy.get("@viewDetailsButton").click();
-
     cy.url().should("contain", "/products/7");
   });
 });
